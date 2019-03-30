18ef2f57a7dec7deb773e08e8467d4d00be37b57
Chalece DeLaCoudray <dahliablack22@gmail.com>
2019-03-30 19:19:26 -0400

    Rect. HIst & Mag.
    Rectangular noise histogram and magnitude spectrum

--- ass3main.m
+++ ass3main.m
@@ -1,5 +1,22 @@
 %%Assignment 3 Main Script. Call Functions, Generate Plots.
 
 r = myNoiseGen(44100,4,'rect');
-t = myNoiseGen(44100,4,'tri');
-h = myNoiseGen(44100,4,'hp');
+
+% hist(rec);
+% title('Rectangular Noise Historgram');
+% xlabel('Amplitude Values');
+% ylabel('Occurences')
+% axis tight
+fs = 44100;
+L = length(r);
+L2 = ceil(L/2);
+
+R = fft(r);
+Rabs = abs(r);
+r_bins = (0:L-1)';
+r_Hz = r_bins * fs/L;
+
+plot(r_Hz(1:L2), Rabs(1:L2))
+
+%tri = myNoiseGen(44100,4,'tri');
+%hp = myNoiseGen(44100,4,'hp');
